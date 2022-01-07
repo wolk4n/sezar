48a94fadb669879abb13f0ae1b9af34156db3c46
wolkan <81369676+wolkann@users.noreply.github.com>
2022-01-07 18:16:52 -0500

    Add files via upload

---
+++ sezar.py
@@ -0,0 +1,37 @@
+def sezarolustur(metin):
+    sifre = ""
+    alfabe=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    for i in metin:
+        sifre+=alfabe[(alfabe.index(i)+3)%26]
+    print("Sezar şifresi :",sifre)
+    return metin
+
+def sezarkir(metin):
+    sifre = ""
+    alfabe=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    for i in metin:
+        sifre+=alfabe[(alfabe.index(i)-3)%26]
+    print("Kırılan şifre :",sifre)
+    return metin
+
+yesil = "\033[92m"
+alti_cizili = '\033[4m'
+kalin = '\033[1m'
+normal = '\033[0m'
+
+print(alti_cizili+yesil+"""---- #Coded By wolkan ----""")
+print(normal+kalin+"""1- Sezar Şifresi Oluştur
+2- Sezar Şifresi Kır""")
+
+soru = input(normal+"Seçenek: ")
+
+if (soru == "1"):
+    isim = input("Metini giriniz (küçük harfler ile) : ")
+    sezarolustur(isim)
+
+elif (soru == "2"):
+    isim = input("Sezar şifrenizi giriniz (küçük harfler ile) : ")
+    sezarkir(isim)
+
+else:
+    print("Hatalı tuşlama yaptınız..!")
